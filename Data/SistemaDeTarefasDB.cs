842f019963fe51a56f85ce0380226f7f99521632
unknown <anderson.lima18pb@gmail.com>
2023-05-08 22:50:26 -0300

    entity-framework

---
+++ Data/SistemaDeTarefasDB.cs
@@ -0,0 +1,25 @@
+﻿using api_c_.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_c_.Data
+{
+    public class SistemaDeTarefasDB : DbContext
+    {
+        public SistemasDeTarefasDB(DbContextOptions<SistemaDeTarefasDB> options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserModel> users { get; set; } 
+        public DbSet<TaskModel> tasks { get; set; }
+
+        protected override void OnMOdelCreating()
+        {
+
+        }
+
+
+
+
+    }
+}
